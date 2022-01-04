0ec60a85e217d2e141b7605a3715ed23b3189622
Thiago <cartaparathiago@gmail.com>
2022-01-04 12:00:52 -0300

    repository tests

---
+++ src/test/java/com/ilia/ponto/repository/PontoRepositoryTest.java
@@ -0,0 +1,70 @@
+package com.ilia.ponto.repository;
+
+
+import com.ilia.ponto.model.Ponto;
+import com.ilia.ponto.model.Usuario;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.annotation.DirtiesContext;
+
+@DataJpaTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+class PontoRepositoryTest {
+
+  @Autowired
+  PontoRepository pontoRepository;
+  @Autowired
+  UsuarioRepository usuarioRepository;
+
+  Usuario usuario = new Usuario(UUID.randomUUID(), "john", "john", null);
+  Ponto ponto = new Ponto(null, LocalDateTime.now(), usuario);
+
+
+
+  @Test
+  @Order(0)
+  void save_Creates_Ponto_When_Successful() {
+    Usuario savesUser = usuarioRepository.save(usuario);
+    Ponto save = pontoRepository.save(ponto);
+
+    Assertions.assertThat(save.getId()).isNotNull().isEqualTo(1);
+
+  }
+
+  @Test
+  void save_ThrowsException_When_UsuarioIsNull() {
+    Ponto ponto = new Ponto(null, LocalDateTime.now(), null);
+
+    Assertions.assertThatExceptionOfType(DataIntegrityViolationException.class)
+        .isThrownBy(() -> pontoRepository.save(ponto));
+
+  }
+
+  @Test
+  void findByUserId_ReturnsPontoList_WhenSuccessful() {
+
+    usuarioRepository.save(usuario);
+    pontoRepository.save(ponto);
+
+    List<Ponto> byUsuarioId = pontoRepository.findByUsuarioId(usuario.getId());
+
+    Assertions.assertThat(byUsuarioId.size()).isEqualTo(1);
+    Assertions.assertThat(byUsuarioId.get(0).getId()).isNotNull();
+
+
+    Assertions.assertThat(ponto.getId()).isNotNull().isEqualTo(1);
+
+  }
+}
---
+++ src/test/java/com/ilia/ponto/repository/UsuarioRepositoryTest.java
@@ -0,0 +1,75 @@
+package com.ilia.ponto.repository;
+
+
+import com.ilia.ponto.model.Ponto;
+import com.ilia.ponto.model.Usuario;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.orm.jpa.JpaSystemException;
+import org.springframework.test.annotation.DirtiesContext;
+
+@DataJpaTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class UsuarioRepositoryTest {
+
+  @Autowired
+  UsuarioRepository usuarioRepository;
+  @Autowired
+      PontoRepository pontoRepository;
+
+  Usuario usuario = new Usuario(UUID.randomUUID(), "john", "john", null);
+  Ponto ponto = new Ponto(null, LocalDateTime.now(), usuario);
+
+
+  @Test
+  @Order(0)
+  void save_Creates_Usuario_When_Successful() {
+    Usuario savesUser = usuarioRepository.save(usuario);
+
+    Assertions.assertThat(savesUser.getId()).isNotNull().isEqualTo(usuario.getId());
+
+  }
+
+  @Test
+  void save_ThrowsException_When_UsuarioIdIsNull() {
+    usuario.setId(null);
+
+    Assertions.assertThatExceptionOfType(JpaSystemException.class)
+        .isThrownBy(() -> usuarioRepository.save(usuario));
+
+  }
+
+  @Test
+  void findByUsername_ReturnsUsuarioOptional_WhenSuccessful() {
+
+    usuarioRepository.save(usuario);
+
+    Optional<Usuario> byUsername = usuarioRepository.findByUsername(usuario.getUsername());
+    Assertions.assertThat(byUsername.get().getId()).isNotNull();
+
+  }
+
+  @Test
+  void findByIdAndPontoLocaDateTime_ReturnsUsuarioOptional_WhenSuccessful() {
+
+    Usuario savedUsuario = usuarioRepository.save(usuario);
+    Ponto savedPonto = pontoRepository.save(this.ponto);
+
+    Optional<Usuario> usuarioFound = usuarioRepository
+        .findByIdAndPontoLocalDateTime(savedUsuario.getId(), savedPonto.getLocalDateTime());
+
+    Assertions.assertThat(usuarioFound.get().getId()).isNotNull();
+
+  }
+}
