d39391a151a7937b8429b8fd7b9076938c1111f4
Thiago <cartaparathiago@gmail.com>
2022-01-05 17:49:20 -0300

    fixing: mudando PontoController.save parametro para @RequestBody

--- src/main/java/com/ilia/ponto/controller/PontoController.java
+++ src/main/java/com/ilia/ponto/controller/PontoController.java
@@ -1,14 +1,14 @@
 package com.ilia.ponto.controller;
 
+import com.ilia.ponto.model.DateInput;
 import com.ilia.ponto.model.Ponto;
 import com.ilia.ponto.service.PontoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,15 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class PontoController {
 
   private final PontoService pontoService;
-  @GetMapping
-  public ResponseEntity<String> ponto(@RequestParam String date) {
-    return new ResponseEntity<>("ponto registrado " + date, HttpStatus.OK);
-  }
-  @PostMapping
-  public ResponseEntity<Ponto> save(@RequestParam String date) {
 
-    Ponto ponto = pontoService.save(date);
+  @PostMapping
+  public ResponseEntity<Ponto> save(@RequestBody DateInput dataHora) {
+
+    Ponto ponto = pontoService.save(dataHora.getDataHora());
     return new ResponseEntity<>(ponto, HttpStatus.CREATED);
   }
-
 }
---
+++ src/main/java/com/ilia/ponto/model/DateInput.java
@@ -0,0 +1,12 @@
+package com.ilia.ponto.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class DateInput {
+  private String dataHora;
+}
