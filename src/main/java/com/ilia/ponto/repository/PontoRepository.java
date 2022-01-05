342713e4660021a955ac1be340267d0549cceed5
Thiago <cartaparathiago@gmail.com>
2022-01-05 17:54:12 -0300

    fixing: horario do almoço validação e refactoring

--- src/main/java/com/ilia/ponto/repository/PontoRepository.java
+++ src/main/java/com/ilia/ponto/repository/PontoRepository.java
@@ -1,25 +1,39 @@
 package com.ilia.ponto.repository;
 
 import com.ilia.ponto.model.Ponto;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface PontoRepository extends JpaRepository<Ponto, Long> {
 
   List<Ponto> findByUsuarioId(UUID id);
 
-  @Query(value = "select case when (count(*) >= 4) then 'true' else 'false' end " +
+  @Query(value = "select case when (count(p) > 3) then 'true' else 'false' end " +
       "from Ponto as p " +
       "inner join Usuario as u " +
       "on u.id = :id " +
-      "and u.id = p.usuario_id " +
-      "where p.localDateTime between " +
-      ":startTime " +
+      "and u = p.usuario " +
+      "where p.localDateTime > " +
+      ":#{#localDateTime.withHour(0).withMinute(0).withSecond(0)} " +
       "and " +
-      " :endTime",
-      nativeQuery = true)
-  boolean isLimitRecordsPerDayReached(String startTime, String endTime, UUID id);
+      "p.localDateTime < :#{#localDateTime.withHour(23).withMinute(59).withSecond(59)}")
+  boolean isLimitRecordsPerDayReached(@Param("localDateTime") LocalDateTime localDateTime, @Param("id") UUID id);
+
+  @Query(value = "select p " +
+      "from Ponto as p " +
+      "inner join Usuario as u " +
+      "on u.id = :id " +
+      "and u = p.usuario " +
+      "where p.localDateTime > " +
+      ":#{#localDateTime.withHour(0).withMinute(0).withSecond(0)} " +
+      "and " +
+      "p.localDateTime < :#{#localDateTime.withHour(23).withMinute(59).withSecond(59)}")
+  List<Ponto> findRecordedPontosOnDayByUserId(@Param("localDateTime") LocalDateTime localDateTime, @Param("id") UUID id);
+
+
 }
--- src/main/java/com/ilia/ponto/service/PontoService.java
+++ src/main/java/com/ilia/ponto/service/PontoService.java
@@ -1,32 +1,44 @@
 package com.ilia.ponto.service;
 
+import com.ilia.ponto.keycloak.KeycloakService;
 import com.ilia.ponto.model.Ponto;
 import com.ilia.ponto.model.Usuario;
 import com.ilia.ponto.repository.PontoRepository;
-import com.ilia.ponto.utils.PontoPersisterUtil;
+import com.ilia.ponto.repository.UsuarioRepository;
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
+import java.util.List;
+import java.util.UUID;
+import javax.ws.rs.BadRequestException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class PontoService {
   private final PontoRepository pontoRepository;
-  private final PontoPersisterUtil pontoPersisterUtil;
+  private final KeycloakService keycloakService;
+  private final UsuarioRepository usuarioRepository;
 
   public Ponto save(String date) {
 
-    LocalDateTime dateTime = pontoPersisterUtil.formatDate(date);
+    LocalDateTime dateTime = formatDate(date);
 
-    Usuario usuario = pontoPersisterUtil.getUsuarioLogado();
+    Usuario usuario = getUsuarioLogado();
 
-    pontoPersisterUtil.validatingNumberOfRecordsPerDay(dateTime, usuario);
+    validatingLunchTime(dateTime, usuario.getId());
 
-    pontoPersisterUtil.notSaturdayNorSundayOrThrowException(dateTime);
+    validatingNumberOfRecordsPerDay(dateTime, usuario.getId());
 
-    pontoPersisterUtil.uniqueDateTimeOrThrowBadRequest(dateTime, usuario);
+    notSaturdayNorSundayOrThrowException(dateTime);
+
+    uniqueDateTimeOrThrowBadRequest(dateTime, usuario);
 
     Ponto ponto = new Ponto(null, dateTime, usuario);
 
@@ -34,4 +46,44 @@ public class PontoService {
   }
 
 
+  private LocalDateTime formatDate(String date) {
+    DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+    return LocalDateTime.parse(date, formatter);
+  }
+
+  private Usuario getUsuarioLogado() {
+    String username = keycloakService.getUsuarioUsername();
+    return usuarioRepository.findByUsername(username)
+        .orElseThrow(() -> new RuntimeException("erro: nenhum usuario encontrado"));
+  }
+
+  private void validatingNumberOfRecordsPerDay(LocalDateTime localDateTime, UUID id) {
+
+    if (pontoRepository.isLimitRecordsPerDayReached(localDateTime, id))
+      throw new BadRequestException("não é possível registrar mais de 4 pontos por dia");
+  }
+
+  private void notSaturdayNorSundayOrThrowException(LocalDateTime localDateTime) {
+    DayOfWeek dayOfWeek = localDateTime.getDayOfWeek();
+    if(dayOfWeek == DayOfWeek.SATURDAY ||
+        dayOfWeek == DayOfWeek.SUNDAY)
+      throw new BadRequestException("Ponto aos sábados e domingos não é permitido");
+  }
+
+  private void uniqueDateTimeOrThrowBadRequest(LocalDateTime localDateTime, Usuario usuario) {
+    if(usuarioRepository.findByIdAndPontoLocalDateTime(usuario.getId(), localDateTime).isPresent())
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "data e hora já constam no banco de dados");
+  }
+
+  private void validatingLunchTime(LocalDateTime localDateTime, UUID id) {
+    List<Ponto> pontos = pontoRepository.findRecordedPontosOnDayByUserId(localDateTime, id);
+
+    if (pontos.size() == 2) {
+      pontos.sort(Comparator.comparing(Ponto::getLocalDateTime));
+      LocalDateTime dateTime = pontos.get(1).getLocalDateTime();
+
+      if (dateTime.isAfter(localDateTime.minusHours(1)))
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Não é permitido menos de 1 hora de almoço");
+    }
+  }
 }
